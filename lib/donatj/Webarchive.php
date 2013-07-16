be5b6e5a68777b87b11a48dd495821ed63cf96a3
Jesse Donat <donatj@gmail.com>
2013-07-16 13:00:40 -0500

    Sub resources fixed

--- lib/donatj/Webarchive.php
+++ lib/donatj/Webarchive.php
@@ -2,6 +2,7 @@
 
 namespace donatj;
 
+use CFPropertyList\CFArray;
 use CFPropertyList\CFData;
 use CFPropertyList\CFDictionary;
 use CFPropertyList\CFPropertyList;
@@ -13,13 +14,18 @@ class Webarchive {
 	 * @var \CFPropertyList\CFPropertyList
 	 */
 	private $plist;
-
 	/**
 	 * @var \CFPropertyList\CFDictionary
 	 */
 	private $dict;
-
+	/**
+	 * @var \CFPropertyList\CFDictionary
+	 */
 	private $mainDoc;
+	/**
+	 * @var \CFPropertyList\CFArray
+	 */
+	private $subDocs;
 
 	function __construct() {
 		$this->plist = new CFPropertyList();
@@ -28,30 +34,47 @@ class Webarchive {
 
 		$this->dict->add('WebMainResource', $this->mainDoc = new CFDictionary());
 
+		$this->dict->add('WebSubresources', $this->subDocs = new CFArray());
+
 	}
 
-	function addMainResource( $content, $url = false, $charset = 'UTF-8', $mime = 'text/html' ) {
+	public function addMainResource( $content, $url = false, $mime = 'text/html', $charset = 'UTF-8', $headers = false ) {
 
-		$this->mainDoc->add('WebResourceData', new CFData($content));
-		$this->mainDoc->add('WebResourceMIMEType', new CFString($mime));
-		$this->mainDoc->add('WebResourceTextEncodingName', new CFString($charset));
+		$this->addDocument($this->mainDoc, $content, $url, $mime, $charset, $headers);
+
+	}
+
+	private function addDocument( CFDictionary $dict, $content, $url, $mime, $charset, $headers ) {
+
+		$dict->add('WebResourceData', new CFData($content));
+		$dict->add('WebResourceMIMEType', new CFString($mime));
+
+		if( $charset ) {
+			$dict->add('WebResourceTextEncodingName', new CFString($charset));
+		}
 
 		if( $url ) {
-			$this->mainDoc->add('WebResourceURL', new CFString($url));
+			$dict->add('WebResourceURL', new CFString($url));
+		}
+
+		if( $headers ) {
+			$dict->add('WebResourceResponse', new CFData($headers));
 		}
 
 	}
 
-	function addSubResources( $content, $url, $charset = 'UTF-8', $mime = 'text/html' ) {
+	public function addSubResource( $content, $url, $mime = 'text/html', $charset = false, $headers = false ) {
+		$this->subDocs->add($dict = new CFDictionary());
 
+		$this->addDocument($dict, $content, $url, $mime, $charset, $headers);
 	}
 
-	function save($filename) {
-		$this->plist->saveBinary( $filename );
+	function save( $filename ) {
+		$this->plist->saveBinary($filename);
 	}
 
 	function output() {
-		$this->plist->saveBinary( 'php://output' );
+		$this->plist->saveBinary('php://output');
 	}
 
 
